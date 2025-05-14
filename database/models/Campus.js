3e0a8f1e805a0526211b4e774bdee87f0e1fa217
AtaurM <ataurmuhith@gmail.com>
2025-05-14 16:47:01 -0400

    Completed campus and student models

--- database/models/Campus.js
+++ database/models/Campus.js
@@ -5,22 +5,37 @@ It defines the campus model for the database.
 ==================================================*/
 const Sequelize = require('sequelize');  // Import Sequelize
 const db = require('../db');  // Import Sequelize database instance called "db"
+const { defaultValueSchemable } = require('sequelize/lib/utils');
 
 // Define the campus model
 const Campus = db.define("campus", {
   name: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
 
   address: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
 
   description: {
+    type: Sequelize.TEXT,
+    allowNull: true
+  },
+
+  imageUrl: {
     type: Sequelize.STRING,
+    allowNull: true,
+    defaultValue: "https://s29068.pcdn.co/wp-content/uploads/campus-shot-768x432.jpg.optimal.jpg"
   }
+
 });
 
 // Export the campus model
--- database/models/Student.js
+++ database/models/Student.js
@@ -9,13 +9,46 @@ const db = require('../db');  // Import Sequelize database instance called "db"
 const Student = db.define("student", {
   firstname: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
 
   lastname: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isEmail: true
+    }
+  },
+
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: true,
+    defaultValue: "https://static.vecteezy.com/system/resources/thumbnails/024/983/914/small_2x/simple-user-default-icon-free-png.png"
+  },
+
+  gpa: {
+    type: Sequelize.DECIMAL(3, 2),
+    allowNull: true,
+    validate: {
+      min: 0.00,
+      max: 4.00
+    }
   }
+
+
+
 });
 
 // Export the student model
